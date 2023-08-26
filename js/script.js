11223c9bcdd72b0831484a81d067d1b538b34331
minarchowdhury <minarhossain0007@gmail.com>
2023-08-26 21:44:07 +0600

    contact page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//start header section
 function handleScroll() {
     let prevScrollPos = window.pageYOffset;
     const header = document.getElementById("header-wrap");
@@ -31,3 +32,30 @@ function toggleMenu() {
     var body = document.body;
     body.classList.toggle("disable-scroll");
 }
+// end header section
+
+// start blog page 
+function changePage(pageNumber) {
+
+    console.log("Page number clicked:", pageNumber);
+
+    const pageNumbers = document.querySelectorAll('.page-number');
+    pageNumbers.forEach(page => page.classList.remove('active-page'));
+
+    const clickedPage = document.querySelector('.page-number:nth-child(' + pageNumber + ')');
+    clickedPage.classList.add('active-page');
+
+    const itemsPerPage = 9;
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const blogItems = document.querySelectorAll('.blog-item');
+
+    blogItems.forEach(item => item.style.display = 'none');
+
+    for (let i = startIndex; i < endIndex && i < blogItems.length; i++) {
+        blogItems[i].style.display = 'inline-block';
+    }
+}
+
+// end blog page
